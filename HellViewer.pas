e86e52c41f66a513d89465ed3e50eff0329518ee
TehNinth <staffofawe@hotmail.co.uk>
2013-07-06 22:38:30 +0100

    Create HellViewer.pas

---
+++ HellViewer.pas
@@ -0,0 +1,49 @@
+unit HellViewer;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, OleCtrls, SHDocVw, StdCtrls, sLabel, ExtCtrls, sPanel;
+
+type
+  THellViewerForm = class(TForm)
+    HellBrowser: TWebBrowser;
+    PanelLoading: TsPanel;
+    LabelLoading: TsLabelFX;
+    Timer: TTimer;
+    procedure FormShow(Sender: TObject);
+    procedure TimerTimer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  HellViewerForm: THellViewerForm;
+  url: string;
+  xoff, yoff : integer;
+
+implementation
+
+{$R *.dfm}
+
+procedure THellViewerForm.FormShow(Sender: TObject);
+begin
+  PanelLoading.Visible := true;
+  LabelLoading.Visible := true;
+  HellBrowser.Navigate('http://bstudios.freehostia.com/viewer/?file=' + url + '&xoff=' + IntToStr(xoff) + '&yoff=' + IntToStr(yoff));
+  Timer.Enabled := true;
+end;
+
+procedure THellViewerForm.TimerTimer(Sender: TObject);
+begin
+  PanelLoading.Visible := false;
+  LabelLoading.Visible := false;
+  Timer.Enabled := false;
+end;
+
+//VERY useless.
+
+end.
